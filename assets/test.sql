cc4c88efea6bc79bb28ed8c1506d35b11362ed3f
n3dhir <nadhir.halbouni5@gmail.com>
2022-04-15 21:34:06 +0100

    added download and upload

--- assets/test.sql
+++ assets/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2022 at 04:29 PM
+-- Generation Time: Apr 15, 2022 at 10:29 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `resources`
+--
+
+CREATE TABLE `resources` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(50) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `year` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resources`
+--
+
+INSERT INTO `resources` (`id`, `filename`, `category`, `subject`, `year`) VALUES
+(12, 'Web.pdf', 'cours', 'web', '1'),
+(13, 'Vision-par-ordi.pdf', 'cours', 'math', '1'),
+(14, 'resources-file.pdf', 'cours', 'math', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -40,11 +63,20 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fname`, `lname`, `mail`, `pass`) VALUES
-(1, 'Nadhir', 'Halbouni', 'nadhir.halbouni5@gmail.com', 'nadhir');
+(1, 'Nadhir', 'Halbouni', 'nadhir.halbouni5@gmail.com', 'nadhir'),
+(15, 'Iheb', 'Daly', 'iheb.daly@etudiant-isi.utm.tn', 'iheb'),
+(17, 'Jane', 'W West', 'cofeway858@bpghmag.com', 'jane'),
+(26, 'Nadhir', 'Halbouni', 'nadhir.halbouni@etudiant-isi.utm.tn', 'nadhir1A');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `resources`
+--
+ALTER TABLE `resources`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -57,10 +89,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `resources`
+--
+ALTER TABLE `resources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
