16fb95addb1ff682bb32af507bed607a941c8374
Dalenza <dalydiiheb@gmail.com>
2022-04-23 14:16:27 +0200

    add scroll bar styles

--- home/home.js
+++ home/home.js
@@ -6,11 +6,11 @@ initSubjectMenu();
 
 const yearSelect = document.getElementById("year");
 yearSelect.addEventListener("change", () => {
-    console.log("change happened")
     switch (yearSelect.value) {
         case '1': updateSubjectsInSelectMenu(firstYearSubjects); break;
         case '2': updateSubjectsInSelectMenu(secondYearSubjects); break;
         case '3': updateSubjectsInSelectMenu(thirdYearSubjects); break;
+        default: initSubjectMenu();
     }
 })
 
