7989d3234d922a97845ec95e275d3c4ea6a99d23
n3dhir <nadhir.halbouni5@gmail.com>
2022-04-24 17:13:05 +0100

    finished the delete and update functionalities"

--- update.php
+++ update.php
@@ -14,6 +14,18 @@
   $query = "SELECT FILENAME FROM RESOURCES WHERE ID = " . $id;
   $res = mysqli_query($conn, $query);
   $oldname = mysqli_fetch_assoc($res);
+  
+  $query = "SELECT filename from RESOURCES";
+  $names = mysqli_fetch_all(mysqli_query($conn, $query));
+  
+  for($i=0;$i<count($names);$i++){
+    if($names[$i][0] == $newname and $newname != $oldname['FILENAME']){
+      $msg = "Sorry, but this name is already used, try another one";
+      $_SESSION['msg'] = $msg;
+      header("location: admin/admin.php");
+      die();
+    }
+  }
   $query = "UPDATE RESOURCES SET filename = '" . $newname . "' , category = '" . $category ."', subject = '" . $subject  . "', year = '" . $year . "' WHERE ID = " . $id;
   if(mysqli_query($conn, $query) and rename('resources/' . $oldname['FILENAME'], 'resources/' . $newname))
     $msg = "Document updated successfully";
