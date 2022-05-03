5a732e70c7635a62b4809af0e5cbe9bb8653bca2
Dalenza <dalydiiheb@gmail.com>
2022-05-03 22:38:16 +0200

    fixed error msgs styling

--- base/renderErrorMessage.php
+++ base/renderErrorMessage.php
@@ -2,8 +2,8 @@
   if (isset($_SESSION['msg'])) {
     echo "<div class='modal'></div>
     <div class='pop-up'>
-    <p style='color: #000;'>" . $_SESSION['msg'] . "</p>
-    <img src='../assets/x-button-327024.png' class='x' onclick='func()'>
+    <p style='color: var(--body-bg-color);'>" . $_SESSION['msg'] . "</p>
+    <img src='../assets/icons8-close.svg' class='x' onclick='func()'>
     </div>";
     unset($_SESSION['msg']);
   }
